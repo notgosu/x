fd2a8676518f3e1cd97326151659a2d8d5e52221
notgosu <not.gosu@gmail.com>
2014-12-03 22:44:36 +0200

    alter user prefs migration

--- rest/backend/modules/user/migrations/m141203_203316_add_demo_user_prefs.php
+++ rest/backend/modules/user/migrations/m141203_203316_add_demo_user_prefs.php
@@ -7,17 +7,9 @@ class m141203_203316_add_demo_user_prefs extends Migration
 {
     public function up()
     {
-		$user = \backend\modules\user\models\User::find()->where('username="demo"')->one();
-	    if ($user){
-		    $user->role = \common\models\User::ROLE_ADMIN;
-		    $user->save(false);
+	    $userPref = \backend\modules\user\models\UserPrefs::find()->where('user_id=:uid', [':uid' => 1])->one();
 
-		    $userPref = \backend\modules\user\models\UserPrefs::find()->where('user_id=:uid', [':uid' => $user->id])->one();
-
-		    if (!$userPref){
-			    $userPref = new \backend\modules\user\models\UserPrefs();
-			    $userPref->user_id = $user->id;
-		    }
+	    if ($userPref){
 		    $userPref->birthday = '1989-08-09';
 		    $userPref->sex = 0;
 		    $userPref->city = 'Киев';
@@ -30,6 +22,7 @@ class m141203_203316_add_demo_user_prefs extends Migration
 
 		    $userPref->save(false);
 	    }
+
     }
 
     public function down()
