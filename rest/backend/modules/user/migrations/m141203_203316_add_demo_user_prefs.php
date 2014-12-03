271bc6ebda46e299dec351092e5c0d4dc2a00263
notgosu <not.gosu@gmail.com>
2014-12-03 22:46:41 +0200

    alter user prefs migration

--- rest/backend/modules/user/migrations/m141203_203316_add_demo_user_prefs.php
+++ rest/backend/modules/user/migrations/m141203_203316_add_demo_user_prefs.php
@@ -5,30 +5,27 @@ use yii\db\Migration;
 
 class m141203_203316_add_demo_user_prefs extends Migration
 {
-    public function up()
-    {
-	    $userPref = \backend\modules\user\models\UserPrefs::find()->where('user_id=:uid', [':uid' => 1])->one();
+	public function up()
+	{
+		$userPref = new \backend\modules\user\models\UserPrefs();
+		$userPref->user_id = 1;
+		$userPref->birthday = '1989-08-09';
+		$userPref->sex = 0;
+		$userPref->city = 'Киев';
+		$userPref->company = 'Vintage';
+		$userPref->appointment = 'Developer';
+		$userPref->short_info = 'Short info';
+		$userPref->phones = json_encode(array('(063) 407-76-15'));
+		$userPref->skype = 'test';
+		$userPref->site = 'test';
 
-	    if ($userPref){
-		    $userPref->birthday = '1989-08-09';
-		    $userPref->sex = 0;
-		    $userPref->city = 'Киев';
-		    $userPref->company = 'Vintage';
-		    $userPref->appointment = 'Developer';
-		    $userPref->short_info = 'Short info';
-		    $userPref->phones = json_encode(array('(063) 407-76-15'));
-		    $userPref->skype = 'test';
-		    $userPref->site = 'test';
+		$userPref->save(false);
+	}
 
-		    $userPref->save(false);
-	    }
+	public function down()
+	{
+		echo "m141203_203316_add_demo_user_prefs cannot be reverted.\n";
 
-    }
-
-    public function down()
-    {
-        echo "m141203_203316_add_demo_user_prefs cannot be reverted.\n";
-
-        return false;
-    }
+		return false;
+	}
 }
